3a09f57229b6fdb3f30814046d70118dfa221dfb
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-06-19 14:28:05 +0545

    Serializers for Chats and reports

--- backend/simplifai/reports/serializers.py
+++ backend/simplifai/reports/serializers.py
@@ -1,2 +1,12 @@
-import rest_framework.serializers as serializers
+from rest_framework.serializers import serializers
+from .models import Chats, Report
 
+class ChatsSeializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chats
+        fields = "__all__"
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = "__all__"
