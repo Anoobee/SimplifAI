b7866a9d7a617d90a1023aa7adbb54e6a2190b09
Ayush <me@example.com>
2024-06-20 23:45:13 +0545

    cors enabled

--- backend/simplifai/simplifai/settings.py
+++ backend/simplifai/simplifai/settings.py
@@ -41,9 +41,13 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'reports.apps.ReportsConfig',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +56,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = "simplifai.urls"
 
